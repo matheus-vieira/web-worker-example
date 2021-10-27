8e862f02b66e32beae226ef8e89733e09f428f90
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2021-10-27 17:10:34 -0300

    comment code to further usage

--- assets/js/swapi/swapi-worker.js
+++ assets/js/swapi/swapi-worker.js
@@ -1,11 +1,11 @@
-const SWAPI_ROUTES = {
-    films: "https://swapi.dev/api/films/",
-    people: "https://swapi.dev/api/people/",
-    planets: "https://swapi.dev/api/planets/",
-    species: "https://swapi.dev/api/species/",
-    starships: "https://swapi.dev/api/starships/",
-    vehicles: "https://swapi.dev/api/vehicles/"
-}
+// const SWAPI_ROUTES = {
+//     films: "https://swapi.dev/api/films/",
+//     people: "https://swapi.dev/api/people/",
+//     planets: "https://swapi.dev/api/planets/",
+//     species: "https://swapi.dev/api/species/",
+//     starships: "https://swapi.dev/api/starships/",
+//     vehicles: "https://swapi.dev/api/vehicles/"
+// }
 
 const API_METHODS = Object.create(null, {
     "films": { enumerable: true, value: getFilms },
